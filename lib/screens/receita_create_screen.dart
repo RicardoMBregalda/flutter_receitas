9e1278037ed1366d28a06dfe4fc7c707d0a2d4a3
RicardoMBregalda <ricardombregalda@gmail.com>
2025-04-09 00:20:34 -0300

    Add tela de create receita, ja vinculado com o list de receita

---
+++ lib/screens/receita_create_screen.dart
@@ -0,0 +1,59 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:receitas_trabalho_2/models/receita.dart';
+
+class ReceitaCreateScreen extends StatelessWidget {
+  static const String routeName = '/receita-create';
+
+  static Route route() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => const ReceitaCreateScreen(),
+    );
+  }
+
+  const ReceitaCreateScreen({super.key});
+
+  void onPressed() {
+    // Implementar a lógica de salvar a receita
+    print("Receita salva!");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController _controllerNome = TextEditingController();
+    final TextEditingController _controllerNota = TextEditingController();
+    final TextEditingController _controllerTempo = TextEditingController();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Criar Receita')),
+      body: Padding(
+        padding: const EdgeInsets.all(8),
+        child: SingleChildScrollView(
+          child: Form(
+            child: Column(
+              children: [
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Nome'),
+                  controller: _controllerNome,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(labelText: 'Nota'),
+                  controller: _controllerNota,
+                ),
+                TextFormField(
+                  decoration: const InputDecoration(
+                    labelText: 'Tempo de Preparo',
+                  ),
+                  controller: _controllerTempo,
+                ),
+                ElevatedButton(onPressed: onPressed, child: Text("Criar")),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
