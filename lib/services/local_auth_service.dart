bfee5d311e554a4d3b08b89fab8c8bbee835a055
RicardoMBregalda <ricardombregalda@gmail.com>
2025-06-12 14:34:57 -0300

    chure: ajuste simples no arquivo

--- lib/services/local_auth_service.dart
+++ lib/services/local_auth_service.dart
@@ -6,27 +6,19 @@ class LocalAuthService {
 
   Future<bool> authenticate(String reason) async {
     try {
-      // Verifica se o dispositivo suporta biometria
       final bool canCheckBiometrics = await _auth.canCheckBiometrics;
       if (!canCheckBiometrics) {
-        // Se não houver biometria, pode-se optar por permitir ou negar.
-        // Neste caso, retornamos 'true' para não bloquear usuários sem sensor.
-        // Ou pode-se retornar 'false' e mostrar uma mensagem.
         return true;
       }
 
-      // Tenta autenticar o usuário
       return await _auth.authenticate(
         localizedReason: reason,
         options: const AuthenticationOptions(
-          biometricOnly:
-              true, // Força o uso apenas de biometria (digital, facial)
-          stickyAuth:
-              true, // Mantém a solicitação na tela se o app for para segundo plano
+          biometricOnly: true,
+          stickyAuth: true,
         ),
       );
     } on PlatformException catch (e) {
-      // Lida com erros, como sensor não disponível ou usuário não cadastrado
       print(e);
       return false;
     }
